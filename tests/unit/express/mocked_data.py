13ff44fe87b2fea21a6fd7c96e7fbea7e4a0c1fd
Basil Dubyk <samitnuk.work@gmail.com>
2020-10-04 13:09:00 +0300

    Update `mocked_data.py` with more data and comments

--- tests/unit/express/mocked_data.py
+++ tests/unit/express/mocked_data.py
@@ -1,4 +1,4 @@
-# request.prefer('return=return=minimal')
+# Returned by `PaymentProcessor.create_order` with request.prefer('return=minimal')
 CREATE_ORDER_RESULT_DATA_MINIMAL = {
     'id': '4MW805572N795704B',
     'links': [
@@ -26,7 +26,8 @@ CREATE_ORDER_RESULT_DATA_MINIMAL = {
     'status': 'CREATED'
 }
 
-# request.prefer('return=representation')
+# Returned by `PaymentProcessor.create_order` with request.prefer('return=representation').
+# Is not used in tests for the moment - shown just as an example.
 CREATE_ORDER_RESULT_DATA_REPRESENTATION = {
     'create_time': '2020-03-05T11:15:33Z',
     'id': '4MW805572N795704B',
@@ -107,6 +108,7 @@ CREATE_ORDER_RESULT_DATA_REPRESENTATION = {
     'status': 'CREATED'
 }
 
+# Returned by `PaymentProcessor.get_order`
 GET_ORDER_RESULT_DATA = {
     'create_time': '2019-12-27T16:39:20Z',
     'id': '4MW805572N795704B',
@@ -187,8 +189,7 @@ GET_ORDER_RESULT_DATA = {
     'status': 'APPROVED'
 }
 
-# TODO does this really minimal?
-# request.prefer('return=minimal')
+# Returned by `PaymentProcessor.capture_order` with request.prefer('return=minimal')
 CAPTURE_ORDER_RESULT_DATA_MINIMAL = {
     'id': '4MW805572N795704B',
     'links': [
@@ -268,6 +269,204 @@ CAPTURE_ORDER_RESULT_DATA_MINIMAL = {
         }
     ],
     'status': 'COMPLETED'
+}
+
+# Returned by `PaymentProcessor.capture_order` with request.prefer('return=representation').
+# Is not used in tests for the moment - shown just as an example.
+CAPTURE_ORDER_RESULT_DATA_REPRESENTATION = {
+    'create_time': '2020-10-04T09:15:37Z',
+    'id': '2SK113799B370635B',
+    'intent': 'CAPTURE',
+    'links': [
+        {
+            'href': 'https://api.sandbox.paypal.com/v2/checkout/orders/2SK113799B370635B',
+            'method': 'GET',
+            'rel': 'self'
+        }
+    ],
+    'payer': {
+        'address': {
+            'country_code': 'GB'
+        },
+        'email_address': 'sherlock.holmes@example.com',
+        'name': {
+            'given_name': 'Sherlock',
+            'surname': 'Holmes'
+        },
+        'payer_id': '0000000000001'
+    },
+    'purchase_units': [
+        {
+            'amount': {
+                'breakdown': {
+                    'handling': {
+                        'currency_code': 'GBP',
+                        'value': '0.00'
+                    },
+                    'insurance': {
+                        'currency_code': 'GBP',
+                        'value': '0.00'
+                    },
+                    'item_total': {
+                        'currency_code': 'GBP',
+                        'value': '0.99'
+                    },
+                    'shipping': {
+                        'currency_code': 'GBP',
+                        'value': '0.00'
+                    },
+                    'shipping_discount': {
+                        'currency_code': 'GBP',
+                        'value': '0.00'
+                    }
+                },
+                'currency_code': 'GBP',
+                'value': '0.99'
+            },
+            'description': 'Google Hacking',
+            'items': [
+                {
+                    'description': '*Author Johnny Long, the '
+                                   'authority on Google hacking, '
+                                   'will be speaking about "Google '
+                                   'Hacking" at the Black Hat 2004 '
+                                   'Briefing.',
+                    'name': 'Google Hacking',
+                    'quantity': '1',
+                    'sku': '9781931836364',
+                    'tax': {
+                        'currency_code': 'GBP',
+                        'value': '0.00'
+                    },
+                    'unit_amount': {
+                        'currency_code': 'GBP',
+                        'value': '0.99'
+                    }
+                }
+            ],
+            'payee': {
+                'email_address': 'seller@example.com',
+                'merchant_id': '1234567890111'
+            },
+            'payments': {
+                'captures': [
+                    {
+                        'amount': {
+                            'currency_code': 'GBP',
+                            'value': '0.99'
+                        },
+                        'create_time': '2020-10-04T09:16:06Z',
+                        'final_capture': True,
+                        'id': '45315376249711632',
+                        'links': [
+                            {
+                                'href': 'https://api.sandbox.paypal.com/v2/payments/captures/45315376249711632',
+                                'method': 'GET',
+                                'rel': 'self'
+                            },
+                            {
+                                'href': 'https://api.sandbox.paypal.com/v2/payments/captures/45315376249711632/refund',
+                                'method': 'POST',
+                                'rel': 'refund'
+                            },
+                            {
+                                'href': 'https://api.sandbox.paypal.com/v2/checkout/orders/2SK113799B370635B',
+                                'method': 'GET',
+                                'rel': 'up'
+                            }
+                        ],
+                        'seller_protection': {
+                            'dispute_categories': [
+                                'ITEM_NOT_RECEIVED',
+                                'UNAUTHORIZED_TRANSACTION'
+                            ],
+                            'status': 'ELIGIBLE'
+                        },
+                        'status': 'PENDING',
+                        'status_details': {
+                            'reason': 'RECEIVING_PREFERENCE_MANDATES_MANUAL_ACTION'
+                        },
+                        'update_time': '2020-10-04T09:16:06Z'
+                    }
+                ]
+            },
+            'reference_id': 'default',
+            'shipping': {
+                'address': {
+                    'address_line_1': '221B Baker Street',
+                    'admin_area_2': 'London',
+                    'country_code': 'GB',
+                    'postal_code': 'WC2N 5DU'},
+                'name': {
+                  'full_name': 'Sherlock Holmes'
+                }
+            },
+            'soft_descriptor': 'PAYPAL *TESTFACILIT'
+        }
+    ],
+    'status': 'COMPLETED',
+    'update_time': '2020-10-04T09:16:06Z'
+}
+
+# Returned by `PaymentProcessor.refund_order` with request.prefer('return=minimal').
+# Is not used in tests for the moment - shown just as an example.
+REFUND_ORDER_DATA_MINIMAL = {
+    'id': '0SM71185A67927728',
+    'links': [
+        {
+            'href': 'https://api.sandbox.paypal.com/v2/payments/refunds/0SM71185A67927728',
+            'method': 'GET',
+            'rel': 'self'
+        },
+        {
+            'href': 'https://api.sandbox.paypal.com/v2/payments/captures/45315376249711632',
+            'method': 'GET',
+            'rel': 'up'
+        }
+    ],
+    'status': 'COMPLETED'
+}
+
+# Returned by `PaymentProcessor.refund_order` with request.prefer('return=representation').
+# Is not used in tests for the moment - shown just as an example.
+REFUND_ORDER_DATA_REPRESENTATION = {
+    'amount': {
+        'currency_code': 'GBP', 'value': '0.99'
+    },
+    'create_time': '2020-10-04T02:51:57-07:00',
+    'id': '0SM71185A67927728',
+    'links': [
+        {
+            'href': 'https://api.sandbox.paypal.com/v2/payments/refunds/0SM71185A67927728',
+            'method': 'GET',
+            'rel': 'self'
+        },
+        {
+            'href': 'https://api.sandbox.paypal.com/v2/payments/captures/45315376249711632',
+            'method': 'GET',
+            'rel': 'up'
+        }
+    ],
+    'seller_payable_breakdown': {
+        'gross_amount': {
+            'currency_code': 'GBP',
+            'value': '0.99'
+        },
+        'net_amount': {
+            'currency_code': 'GBP',
+            'value': '0.99'
+        },
+        'paypal_fee': {
+            'currency_code': 'GBP',
+            'value': '0.00'
+        },
+        'total_refunded_amount': {
+            'currency_code': 'GBP',
+            'value': '0.99'
+        }
+    },
+    'status': 'COMPLETED',
+    'update_time': '2020-10-04T02:51:57-07:00'
 }
 
 GET_ORDER_AUTHORIZE_RESULT_DATA = {
